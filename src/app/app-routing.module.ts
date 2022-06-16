27c4eab8b3a53c7e3c1d415e817e571b3418b660
ahmedAISSA <isahmedaissa@gmail.com>
2022-06-16 20:25:09 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ResetComponent } from './auth/reset/reset.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+    {path:'',redirectTo:'signin',pathMatch:'full'},
+    {path:'signin',component:SigninComponent},
+    {path:'signup',component:SignupComponent},
+    {path:'reset',component:ResetComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
